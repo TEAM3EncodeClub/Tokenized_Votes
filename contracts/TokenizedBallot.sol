4265b2084313c99089efd974d2ce9d3292bac2ec
David E. Perez Negron R <david@neetsec.com>
2023-03-05 22:14:19 -0600

    Adding TokenizedBallot.sol

--- contracts/TokenizedBallot.sol
+++ contracts/TokenizedBallot.sol
@@ -1,22 +1,42 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
+interface IMyToken{
+    function getPastVotes(address account, uint256 blockNumber) external view returns (uint256); 
+}
 contract Ballot {
     struct Proposal {
         bytes32 name;
         uint voteCount;
     }
 
+    IMyToken public tokenContract; 
+    uint256 public targetBlockNumber;
     Proposal[] public proposals;
+    mapping (address => uint256) public votingPowerSpent;
 
-    constructor(bytes32[] memory proposalNames) {
+    constructor(
+        bytes32[] memory proposalNames,
+        address _tokenContract,
+        uint256 _targetBlockNumber
+    ) {
+        tokenContract = IMyToken(_tokenContract);
+        targetBlockNumber = _targetBlockNumber;
         for (uint i = 0; i < proposalNames.length; i++) {
             proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
         }
+        
     }
 
-    function vote(uint proposal) external {
-        //TODO
+    function vote(uint proposal, uint256 amount) external {
+        require(votingPower(msg.sender) >= amount);
+        votingPowerSpent[msg.sender] += amount;
+        proposals[proposal].voteCount += amount;
+    }
+
+    function votingPower(address account) public view returns (uint256){
+        return tokenContract.getPastVotes(account, targetBlockNumber) -
+            votingPowerSpent[account];
     }
 
     function winningProposal() public view returns (uint winningProposal_) {
